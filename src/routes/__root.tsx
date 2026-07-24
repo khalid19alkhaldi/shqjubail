477b4e3dca71ad3603a0cd1608b4d717a4f51f58
@k1s1au1 <k1s1au1@gmail.com>
2026-07-24 18:26:00 +0300

    Add Toaster to root to fix login issues

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -12,6 +12,7 @@ import { useEffect, type ReactNode } from "react";
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { AuthProvider, useAuth } from "@/hooks/use-auth";
+import { Toaster } from "@/components/ui/sonner";
 
 function NotFoundComponent() {
   return (
@@ -130,6 +131,7 @@ function RootShell({ children }: { children: ReactNode }) {
       </head>
       <body>
         {children}
+        <Toaster />
         <Scripts />
       </body>
     </html>
